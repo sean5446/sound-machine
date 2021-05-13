638c8f52f528ffb49e0f03abb1677070f6fb2c37
Sean <sean5446@gmail.com>
2021-05-13 15:58:58 -0400

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -7,9 +7,13 @@ if [ ! -z "$1" ]; then
     SOUND_FILE=$1
 fi
 
-while [ 1 ]; do
-  #omxplayer $DIR/$SOUND_FILE.mp3
-  vlc -R --intf dummy $DIR/$SOUND_FILE.mp3
-  sleep 0.1
-done
+# old pi add:
+#  -A alsa to cvlc command
+#  remove the force_hdmi from /boot/config.txt 
 
+cvlc -R --intf dummy $DIR/$SOUND_FILE.mp3
+
+#while [ 1 ]; do
+#  omxplayer $DIR/$SOUND_FILE.mp3
+#  sleep 0.1
+#done
